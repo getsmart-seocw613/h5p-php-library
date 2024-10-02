3c0a2c866040118da427b32dc57a7dae4c4aed0d
getsmart-seocw613 <seocw613@getsmart.co.kr>
2024-10-02 10:01:14 +0900

    [Docs] index

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 // 오류 표출 여부 설정 (0: 미표출, 1: 표출)
 ini_set('display_errors', 1);
-
-require_once './h5p.classes.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,17 +8,12 @@ require_once './h5p.classes.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./styles/h5p.css">
-    <script src="./js/h5p-resizer.js"></script>
-    <script src="./js/h5p.js"></script>
+    <script src="https://h5p.org/sites/all/modules/h5p/library/js/h5p-resizer.js" charset="UTF-8"></script>
 </head>
 <body>
     <div class="h5p-iframe-wrapper" style="width: 800px;">
         <iframe src="https://h5p.org/h5p/embed/617" width="1090" height="675" frameborder="0" allowfullscreen="allowfullscreen" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" title="Interactive Video"></iframe>
     </div>
-<?php
-// H5P 라이브러리의 버전 정보 출력
-echo 'H5P Library loaded successfully. Version: ' . H5PCore::$coreApi['majorVersion'] . '.' . H5PCore::$coreApi['minorVersion'];
-?>
+    <iframe src="https://h5p.org/h5p/embed/27611" width="1090" height="675" frameborder="0" allowfullscreen="allowfullscreen" allow="geolocation *; microphone *; camera *; midi *; encrypted-media *" title="Animal sound game"></iframe>
 </body>
 </html>
